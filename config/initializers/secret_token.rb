53e7994b2fba17fc640a00791e6146bebdfb4270
Nikhil P Agrawal <nikhil.pa@gmail.com>
2013-05-17 20:52:41 +0530

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freemindscafe::Application.config.secret_token = '1dca70eba0a3e4f15167e045cccb00cd3f91a7caa5b2efa746d834c2d579d3631b5091926f52779332b21eeaf8026836055fc9813496b61d6d3a2c9369ee364b'
